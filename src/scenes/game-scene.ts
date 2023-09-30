7049e5e88524fea711c132249b2dd37a33e6bf5f
Bookseller <bookseller@book-seller.xyx>
2023-09-30 01:01:26 -0500

    Top wall can now squish you

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -2,6 +2,12 @@ import * as Phaser from 'phaser';
 
 export class GameScene extends Phaser.Scene {
 
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys
+  player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+  platforms: Phaser.Physics.Arcade.StaticGroup
+  walls: Phaser.Physics.Arcade.Group
+  topWall: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
+
   constructor() {
     super({
       key: 'GameScene'
@@ -13,17 +19,23 @@ export class GameScene extends Phaser.Scene {
   create(): void {
     this.add.image(400, 300, 'sky');
 
-    const platforms: Phaser.Physics.Arcade.StaticGroup = this.physics.add.staticGroup();
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.walls = this.physics.add.group();
+    // The wall sprite is 400x32
+    this.topWall = this.physics.add.image(400, 16, 'wall').setImmovable(true)
+    this.topWall.setScale(2, 1)
+    this.topWall.body.setAllowGravity(false);
+    // This breaks everything. Weird. 
+    // this.walls.add(topWall)
 
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-    var player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody = this.physics.add.sprite(100, 450, 'dude');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: 'left',
@@ -45,11 +57,34 @@ export class GameScene extends Phaser.Scene {
       repeat: -1
     });
 
-    const cursors: Phaser.Types.Input.Keyboard.CursorKeys = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(this.player, this.topWall);
+    this.physics.add.collider(this.player, this.platforms);
   }
 
   update(): void {
+    this.topWall.setY(this.topWall.y + .1)
 
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play('right', true);
+    }
+    else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 
 }
