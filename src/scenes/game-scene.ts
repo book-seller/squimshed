d4c2de671a30fff8a2950e084464a01eaa3a492e
Bookseller <bookseller@book-seller.xyx>
2023-09-30 21:00:48 -0500

    crushing wall stuff

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -7,7 +7,8 @@ export class GameScene extends Phaser.Scene {
   platforms: Phaser.Physics.Arcade.StaticGroup
   walls: Phaser.Physics.Arcade.Group
   coins: Phaser.GameObjects.Group
-  topWall: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
+  // topWall: Phaser.Types.Physics.Arcade.ImageWithDynamicBody
+  topWall: Phaser.GameObjects.TileSprite
 
   constructor() {
     super({
@@ -31,20 +32,8 @@ export class GameScene extends Phaser.Scene {
 
     this.coins = this.add.group({
       defaultKey: 'coin',
-      /*
-      maxSize: 100,
-      createCallback: function (alien) {
-        alien.setName(`alien${this.getLength()}`);
-        console.log('Created', alien.name);
-      },
-      removeCallback: function (alien) {
-        console.log('Removed', alien.name);
-      }
-      */
     });
     
-    // this.coins.create(62, 200)
-
     this.coins = this.physics.add.group({
       key: 'coin',
       setXY: { x: 62, y: 150 },
@@ -52,14 +41,20 @@ export class GameScene extends Phaser.Scene {
     })
 
     this.coins.create(550, 440, 'coin')
+    this.coins.create(720, 300, 'coin')
+    this.coins.create(690, 150, 'coin')
 
-    this.walls = this.physics.add.group();
-    // The wall sprite is 400x32
-    this.topWall = this.physics.add.image(400, 16, 'wall').setImmovable(true)
-    this.topWall.setScale(2, 1)
-    this.topWall.body.setAllowGravity(false);
+    this.walls = this.physics.add.group({
+      allowGravity: false,
+      immovable: true
+    });
+    // The wall sprite is 400x32 <- now it is 100 x 40
+    // this.topWall = this.physics.add.image(400, 16, 'wall').setImmovable(true)
+    this.topWall = this.add.tileSprite(400, 16, 800, 40, 'wall')
+    // this.topWall.setScale(2, 1)
+    // this.topWall.body.setAllowGravity(false);
     // Adding a wall to the walls group breaks everything. Weird. 
-    // this.walls.add(topWall)
+    this.walls.add(this.topWall)
 
     this.player = new Player(this, 100, 450);
 
@@ -74,7 +69,7 @@ export class GameScene extends Phaser.Scene {
 
   update(): void {
     // Move walls
-    // this.topWall.setY(this.topWall.y + .1)
+    this.topWall.setY(this.topWall.y + .1)
     // Whyyyyyyy???!!!!
     this.player.update()
   }
