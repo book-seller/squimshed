cf47963f8d52db6930317d62b8498293c7717427
Bookseller <bookseller@book-seller.xyx>
2023-10-02 00:45:47 -0500

    core gameplay seems to work

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -10,6 +10,7 @@ export class GameScene extends Phaser.Scene {
   topWall: Phaser.GameObjects.TileSprite
   bottomWall: Phaser.GameObjects.TileSprite
   leftWall: Phaser.GameObjects.TileSprite
+  rightWall: Phaser.GameObjects.TileSprite
   woodPlatforms: Phaser.Physics.Arcade.Group
   coinsLeftText: Phaser.GameObjects.Text
   // Colliders
@@ -24,7 +25,7 @@ export class GameScene extends Phaser.Scene {
   }
 
   init(): void {
-    // TODO: May need to initialize a lot of our fields as empty here so we can easily restart the level on death
+    // Initialize a lot of our fields as empty here so we can easily restart the level on death
     // Sprites
     this.stonePlatforms = undefined
     this.coins = undefined
@@ -32,6 +33,7 @@ export class GameScene extends Phaser.Scene {
     this.topWall = undefined
     this.bottomWall = undefined
     this.leftWall = undefined
+    this.rightWall = undefined
     this.woodPlatforms = undefined
     this.player = undefined
     this.coinsLeftText = undefined
@@ -45,7 +47,6 @@ export class GameScene extends Phaser.Scene {
     this.cameras.main.setBounds(0, 0, 1000, 700);
     this.physics.world.setBounds(0, 0, 1000, 700);
     // Stone Platforms
-    // this.stonePlatforms = this.physics.add.staticGroup();
     this.stonePlatforms = this.physics.add.group({
       allowGravity: false,
       immovable: true
@@ -68,7 +69,7 @@ export class GameScene extends Phaser.Scene {
 
     this.coins = this.physics.add.group({
       key: 'coin',
-      setXY: { x: 62, y: 150 },
+      setXY: { x: 262, y: 150 },
       allowGravity: false
     })
 
@@ -86,6 +87,8 @@ export class GameScene extends Phaser.Scene {
     this.walls.add(this.bottomWall)
     this.leftWall = this.add.tileSprite(0, 400, 40, 800, 'wall')
     this.walls.add(this.leftWall)
+    this.rightWall = this.add.tileSprite(940, 400, 40, 800, 'wall')
+    this.walls.add(this.rightWall)
 
     this.player = new Player(this, 100, 450);
 
@@ -98,31 +101,6 @@ export class GameScene extends Phaser.Scene {
     this.playerStoneCollider = this.physics.add.collider(this.player, this.stonePlatforms);
     this.playerWoodCollider = this.physics.add.collider(this.player, this.woodPlatforms);
 
-    /*
-    this.physics.world.on('collide', (gameObject1, gameObject2) => {
-      let collidedPlayer
-      let collidedTopWall
-
-      if (gameObject1 == this.player && gameObject2 == this.topWall) {
-        console.log('[game-scene -> collide check] gameObject1 == this.player && gameObject2 == this.topWall')
-        collidedPlayer = gameObject1
-        collidedTopWall = gameObject2
-      }
-      if (gameObject1 == this.topWall && gameObject2 == this.player) {
-        console.log('[game-scene -> collide check] gameObject1 == this.topWall && gameObject2 == this.player')
-        collidedTopWall = gameObject1
-        collidedPlayer = gameObject2
-      }
-
-      if (collidedPlayer && collidedTopWall && this.player.body.touching.up) {
-        console.log('Touching up on wall - squimsh potential')
-      }
-
-      gameObject1.setAlpha(0.5);
-      gameObject2.setAlpha(0.5);
-    });
-    */
-
     this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
     this.physics.add.overlap(this.player, this.walls, this.playerWallOverlapCheck, null, this);
     this.physics.add.overlap(this.player, this.stonePlatforms, this.playerPlatformOverlapCheck, null, this);
@@ -134,6 +112,7 @@ export class GameScene extends Phaser.Scene {
     this.topWall.setY(this.topWall.y + .1)
     this.bottomWall.setY(this.bottomWall.y - .1)
     this.leftWall.setX(this.leftWall.x + .1)
+    this.rightWall.setX(this.rightWall.x - .1)
     // Whyyyyyyy???!!!!
     this.player.update()
     // Death animation 
@@ -143,12 +122,15 @@ export class GameScene extends Phaser.Scene {
   }
 
   playerWallCollideCheck(gameObject1, gameObject2): void {
+    // Attempt to "help" Phaser's poor physics code
     if (gameObject1 == this.leftWall || gameObject2 == this.leftWall) {
-      console.log('playerWallCollideCheck() -> player is colliding with left wall')
-      // Attempt to "help" Phaser's poor physics code
       if (this.player.getLeftCenter().x < this.leftWall.getRightCenter().x) {
-        console.log('playerWallCollideCheck() -> trying to push player to right')
         this.player.setX(this.leftWall.getRightCenter().x + (this.player.width / 2));
+      }
+    }
+    if (gameObject1 == this.rightWall || gameObject2 == this.rightWall) {
+      if (this.player.getRightCenter().x > this.rightWall.getLeftCenter().x) {
+        this.player.setX(this.rightWall.getLeftCenter().x - (this.player.width / 2));
       }
     }
   }
@@ -158,20 +140,10 @@ export class GameScene extends Phaser.Scene {
   }
 
   playerWallOverlapCheck(gameObject1, gameObject2): void {
-      // console.log('Squimshed!');
-      if (gameObject1 == this.topWall || gameObject2 == this.topWall) {
-        // topWall overlaps work
-        this.die();
-      } else {
-        // Need to do a more precise check for actual overlap of bottomWall
-        // const _bottomWall: Phaser.GameObjects.TileSprite = gameObject1 == this.bottomWall ? gameObject1 : gameObject2
-        /*
-        const _bottomWallTopY = this.bottomWall.getTopCenter().y
-        const _playerBottomY = this.player.getBottomCenter().y
-        console.log(`playerWallOverlapCheck() -> Overlapping bottomWall | _bottomWallTopY = ${_bottomWallTopY} | _playerBottomY = ${_playerBottomY}`)
-        */
-      }
-      
+    if (gameObject1 == this.topWall || gameObject2 == this.topWall) {
+      // topWall overlaps work
+      this.die()
+    }
   }
 
   collectCoin(player, coin): void {
@@ -182,7 +154,6 @@ export class GameScene extends Phaser.Scene {
     this.coinsLeftText.setText('Coins Left: ' + this.coins.countActive(true))
     if (this.coins.countActive(true) < 1) {
       // Move to next scene
-      // Scene.shutdown()
       this.scene.start()
     }
   }
@@ -203,9 +174,7 @@ export class GameScene extends Phaser.Scene {
 
   playOutDeath(): void {
     if (!Phaser.Geom.Rectangle.Overlaps(this.physics.world.bounds, this.player.getBounds())) {
-      // console.log('update() -> Dead Player is out of bounds')
       if (this.player.y > this.physics.world.bounds.height * 2) {
-        // console.log('update() -> Dead Player has fallen out of the world. Restarting scene.')
         this.scene.start()
       }
     }
